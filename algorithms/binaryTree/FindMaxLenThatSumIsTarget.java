44064d69c7d058e09d2ace3974be7a7c1d3ff40b
gayyou <zxc365912613@qq.com>
2020-02-23 19:19:22 +0800

    20200223

---
+++ algorithms/binaryTree/FindMaxLenThatSumIsTarget.java
@@ -0,0 +1,86 @@
+package algorithms.binaryTree;
+
+import model.BinaryNode;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author Weybn
+ * @motto Rare in the world you're worth it.
+ * @time 2020/2/23 13:56
+ */
+public class FindMaxLenThatSumIsTarget {
+  public static int getMaxLen(BinaryNode<Integer> root, int targetNum) {
+    if (root == null) {
+      return 0;
+    }
+
+    HashMap<Integer, Integer> hashMap = new HashMap<>();
+    if (root.getVal() == targetNum) {
+      hashMap.put(0, 1);
+    } else {
+      hashMap.put(0, 0);
+    }
+
+    preOrder(root.getLeft(), hashMap, 1, root.getVal(), targetNum);
+    preOrder(root.getRight(), hashMap, 1, root.getVal(), targetNum);
+
+    int max = -1;
+    for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+      if (entry.getValue() > 0) {
+        max = Math.max(entry.getKey() + 1, max);
+      }
+    }
+
+    return max;
+  }
+
+  private static void preOrder(BinaryNode<Integer> temp, HashMap<Integer, Integer> hashMap,
+                               int level, int preSum, int target) {
+    if (temp == null) {
+      return ;
+    }
+
+    if (!hashMap.containsKey(level)) {
+      hashMap.put(level, 0);
+    }
+
+    int curSum = preSum + temp.getVal();
+
+    if (preSum + temp.getVal() == target) {
+      hashMap.put(level, hashMap.get(level) + 1);
+    }
+
+    preOrder(temp.getLeft(), hashMap, level + 1, curSum, target);
+    preOrder(temp.getRight(), hashMap, level + 1, curSum, target);
+  }
+
+  public static void main(String[] args) {
+    BinaryNode<Integer> binaryNode1 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode2 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode3 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode4 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode5 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode6 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode7 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode8 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode9 = new BinaryNode<>(-1);
+    BinaryNode<Integer> binaryNode10 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode11 = new BinaryNode<>(1);
+    BinaryNode<Integer> binaryNode12 = new BinaryNode<>(1);
+    binaryNode6.setLeft(binaryNode2);
+    binaryNode6.setRight(binaryNode3);
+    binaryNode2.setLeft(binaryNode1);
+    binaryNode2.setRight(binaryNode4);
+    binaryNode3.setRight(binaryNode5);
+    binaryNode5.setRight(binaryNode7);
+    binaryNode4.setRight(binaryNode8);
+    binaryNode4.setLeft(binaryNode9);
+    binaryNode9.setLeft(binaryNode10);
+    binaryNode10.setLeft(binaryNode11);
+    binaryNode5.setLeft(binaryNode12);
+    System.out.println(FindMaxLenThatSumIsTarget.getMaxLen(binaryNode6, 4));
+  }
+}
--- algorithms/linkedlist/LinkedListMergeSort.java
+++ algorithms/linkedlist/LinkedListMergeSort.java
@@ -1,9 +1,90 @@
 package algorithms.linkedlist;
 
+import model.LinkedListNode;
+
 /**
  * @author Weybn
  * @motto Rare in the world you're worth it.
  * @time 2020/2/22 13:43
  */
 public class LinkedListMergeSort {
+  static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+  public ListNode sortList(ListNode head) {
+    // 归并排序
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    // 使用快慢法来进行寻找中间节点
+    ListNode slow = head, fast = head.next;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+
+    // 寻找到中间节点然后拆分链表
+    ListNode right = sortList(slow.next);
+    slow.next = null;
+    ListNode left = sortList(head);
+
+    // 接下来是合并两个有序链表的操作
+    ListNode newHead;
+
+    if (left.val < right.val) {
+      newHead = left;
+    } else {
+      newHead = right;
+      ListNode temp = left;
+      left = right;
+      right = temp;
+    }
+
+    ListNode pre = left, next;
+
+    while (left != null && right != null) {
+      if (left.val <= right.val) {
+        pre = left;
+        left = left.next;
+      } else {
+        pre.next = right;
+        next = right.next;
+        right.next = left;
+        pre = right;
+        right = next;
+      }
+    }
+
+    pre.next = left == null ? right : left;
+
+    return newHead;
+  }
+
+  public static void main(String[] args) {
+    ListNode listNode1 = new ListNode(4);
+    ListNode listNode2 = new ListNode(19);
+    ListNode listNode3 = new ListNode(14);
+    ListNode listNode4 = new ListNode(5);
+    ListNode listNode5 = new ListNode(-3);
+    ListNode listNode6 = new ListNode(1);
+    ListNode listNode7 = new ListNode(8);
+    ListNode listNode8 = new ListNode(5);
+    ListNode listNode9 = new ListNode(11);
+    ListNode listNode10 = new ListNode(15);
+    listNode1.next = listNode2;
+    listNode2.next = listNode3;
+    listNode3.next = listNode4;
+    listNode4.next = listNode5;
+    listNode5.next = listNode6;
+    listNode6.next = listNode7;
+    listNode7.next = listNode8;
+    listNode8.next = listNode9;
+    listNode9.next = listNode10;
+    LinkedListMergeSort linkedListMergeSort = new LinkedListMergeSort();
+    System.out.println(linkedListMergeSort.sortList(listNode1).next);
+  }
 }
